5b41ca904d22d6ff48afabd47d53c9d50c3e061f
RudyMas <rudy.mas@rudymas.be>
2016-11-29 21:11:23 +0100

    Angular Dag 2 (Klas)

---
+++ H3/artikelApp.js
@@ -0,0 +1,13 @@
+var app = angular.module('artikelApp', []);
+app.controller('artikelController', function ($scope){
+    $scope.artikelen = [
+        {artId: '000001', omschrijf: 'Tandenborstel', verkoopPrijs: 3.25, aankoopPrijs: 1.50},
+        {artId: '000002', omschrijf: 'Handdoek', verkoopPrijs: 3.25, aankoopPrijs: 2.50},
+        {artId: '000003', omschrijf: 'Washandje', verkoopPrijs: 4.75, aankoopPrijs: 0.40},
+        {artId: '000004', omschrijf: 'Keukenrol', verkoopPrijs: 1.20, aankoopPrijs: 0.65},
+        {artId: '000005', omschrijf: 'Tandpasta', verkoopPrijs: 0.65, aankoopPrijs: 0.15}
+    ];
+    $scope.clickMe = function(artikel) {
+            alert('Something');
+    };
+});
