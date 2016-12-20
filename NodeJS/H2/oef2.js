142c9d63debfaf847ef1f9ae40a1638b2d61da7a
RudyMas <rudy.mas@rudymas.be>
2016-12-20 17:30:02 +0100

    Node Hoofdstuk 2 Oefening 2

---
+++ NodeJS/H2/oef2.js
@@ -0,0 +1,15 @@
+var voornaam = "Peter";
+var achternaam = "Kassenaar";
+var email = "info@kassenaar.com";
+var http = require('http');
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'application/json'});
+    response.write('{');
+    response.write('"voornaam":"'+voornaam+'",');
+    response.write('"achternaam":"'+achternaam+'",');
+    response.write('"email":"'+email+'"');
+    response.write('}');
+    response.end();
+});
+server.listen(3000);
+console.log('Server gestart op http://localhost:3000 ...');
