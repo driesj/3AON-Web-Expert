8e9704dcb6a962d5f1e1f25c63cf57a6108bcde4
RudyMas <rudy.mas@rudymas.be>
2017-02-03 08:25:09 +0100

    Cleaning code

--- Syntheseoefening/AngularJS/services/dbFactory.js
+++ Syntheseoefening/AngularJS/services/dbFactory.js
@@ -16,40 +16,6 @@
             return $http.get('data/opties.json');
         };
 
-
-
-        factory.getAuto = function (id) {
-            return $http({
-                method: 'GET',
-                url: 'http://localhost/webexpert/AngularJS/H6/API/auto/'+id
-            })
-        };
-
-        factory.deleteAuto = function (id) {
-            return $http({
-                method: 'POST',
-                url: 'http://localhost/webexpert/AngularJS/H6/API/deleteAuto/'+id
-            })
-        };
-
-        factory.addAuto = function (newAuto) {
-            return $http({
-                method: 'POST',
-                url: 'http://localhost/webexpert/AngularJS/H6/API/newAuto',
-                data: newAuto,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-        };
-
-        factory.getTypeAutos = function () {
-            return $http({
-                method: 'GET',
-                url: 'http://localhost/webexpert/AngularJS/H6/API/typeAutos'
-            })
-        };
-
         return factory;
     }
 }) ();
